c6d903a794d7cbfbf3c2a1e257b8fb3b6ff50fcc
Chun <acrobaticat@gmail.com>
2018-01-18 12:21:38 +0800

    updated some files

--- {{cookiecutter.module_name}}/src/main.py
+++ {{cookiecutter.module_name}}/src/main.py
@@ -1,11 +1,13 @@
 
 import os
 import sys
+
 # sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.dirname(__file__))
 
 
 class {{cookiecutter.module_name}}(object):
+
     def __init__(self, feed={}):
         '''
 
@@ -27,7 +29,7 @@ class {{cookiecutter.module_name}}(object):
        :return:
        '''
 
-    def load_model(self, feed={}):
+    def load_model(self, file=self.checkpoint_dir+"/{{cookiecutter.module_name}}.pkl"):
         '''
 
         :param feed:
